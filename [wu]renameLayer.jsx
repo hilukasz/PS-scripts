82d15e77d6d2d75b2888606f5797ad5af7b456d3
Łukasz Wieczorek <lukasz@hellowoo.com>
2015-03-18 17:17:01 -0700

    added requested feature
    get name of current layer before changing name.

--- [wu]renameLayer.jsx
+++ [wu]renameLayer.jsx
@@ -1,9 +1,10 @@
 ﻿// written by Lukasz Wieczorek at http://lukasz.io
 // Please do not remove this and give credit if you repost. Thanks!
-
-var myName=prompt("new name of layer","");
-
-var layerLength = app.activeDocument.layers.length;
 var docRef  = app.activeDocument;
+
+
+var currentName = docRef.activeLayer.name;
+var myName=prompt("new name of layer",currentName);
+var layerLength = app.activeDocument.layers.length;
 
 docRef.activeLayer.name = myName;
