d9f8b1cee9891d78ff5c46b61f7ca988ecf515e0
Łukasz Wieczorek <lukasz@hellowoo.com>
2015-03-18 17:05:42 -0700

    updated incorrect prompt name

--- [wu]renameLayer.jsx
+++
@@ -1,9 +0,0 @@
-﻿// written by Lukasz Wieczorek at http://lukasz.io
-// Please do not remove this and give credit if you repost. Thanks!
-
-var myName=prompt("Font color","text size");
-
-var layerLength = app.activeDocument.layers.length;
-var docRef  = app.activeDocument;
-
-docRef.activeLayer.name = myName;
