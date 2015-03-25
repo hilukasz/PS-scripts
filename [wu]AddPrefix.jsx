bf8cd8f495be48efd986fa610dc9aa3c218d9d25
Łukasz Wieczorek <lukasz@hellowoo.com>
2015-03-24 18:15:37 -0700

    init commit

---
+++ [wu]AddPrefix.jsx
@@ -0,0 +1,14 @@
+﻿// written by Lukasz Wieczorek at http://lukasz.io
+// Please do not remove this and give credit if you repost. Thanks!
+
+var layerLength = app.activeDocument.layers.length;
+var docRef  = app.activeDocument;
+
+var myPrefix=prompt("enter prefix","");
+
+for(i = 0; i < layerLength; i++){
+   docRef.activeLayer = docRef.layers[i]
+   var myName = app.activeDocument.activeLayer.name;
+   var newName = myPrefix+myName;
+   docRef.activeLayer.name = newName;
+ }
--- [wu]renameLayer.jsx
+++ [wu]renameLayer.jsx
@@ -2,7 +2,6 @@
 // Please do not remove this and give credit if you repost. Thanks!
 var docRef  = app.activeDocument;
 
-
 var currentName = docRef.activeLayer.name;
 var myName=prompt("new name of layer",currentName);
 var layerLength = app.activeDocument.layers.length;
