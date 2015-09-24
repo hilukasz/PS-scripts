f063fd54bb34b25d8c73687f43fcfc2cdaee3dc7
Łukasz Wieczorek <lukasz@hellowoo.com>
2015-09-24 14:24:07 -0700

    new files

---
+++ mirror.jsx
@@ -0,0 +1,78 @@
+﻿
+#targetengine main;  
+  
+var pfad = "C:\Program Files\Adobe\Adobe Illustrator CC 2014\Presets\en_US\Scripts"  
+  
+var win = new Window('palette', 'Lukasz\'s super badass panel of awesome');  
+var tpanel = win.add ("tabbedpanel");  
+tpanel.alignChildren = ["fill", "fill"];  
+tpanel.preferredSize = [100,20];  
+win.margins = 0;  
+win.spacing = 0;  
+var subtab1panel = tpanel.add ("tab", undefined, "Custom Art Scripts");  
+subtab1panel.orientation="row";  
+subtab1panel.alignChildren = ["fill", "fill"];  
+  
+  
+var t1group = subtab1panel.add("group");  
+t1group.orientation = "column";  
+t1group.alignChildren = ["fill", "fill"];  
+  
+  
+var btnSelect1 = t1group.add('button', undefined, 'Mirror on Horz Axis');  
+//var scriptToLoad1 = new File("C:\Program Files\Adobe\Adobe Illustrator CC 2014\Presets\en_US\Scripts\mirrorOnRightAxis.jsx");  
+  
+var btnSelect2 = t1group.add('button', undefined, 'Omni Ungroup');   
+var scriptToLoad2 = new File(pfad+"mirrorOnRightAxis.jsx");   
+  
+  
+  
+btnSelect1.onClick = function(){  
+      
+    flipHorizontally();
+    flipHorizontally(duplicateObject());
+    moveObject();
+
+    function moveObject(){
+        var idoc = app.activeDocument;  
+        var sel = idoc.selection[0];  
+        sel.left = sel.left+sel.width;  
+    }
+
+    function duplicateObject(){
+        selection = app.activeDocument.selection;
+        newItem = selection[0].duplicate();
+        return newItem;
+    }
+
+    function flipHorizontally(objectToFlip) {
+        mySelection = objectToFlip;
+        var doc = app.activeDocument;              
+        var s    = doc.selection;                            
+        var sl   = s.length;                                    
+        var m  = app.getScaleMatrix(-100,100);      
+        for(var i = 0 ; i < sl; i++) s[i].transform(m);
+        app.redraw();
+    }
+
+}// end function  
+  
+  
+btnSelect2.onClick = function(){  
+      
+   var des = scriptToLoad2;  
+   des.open("r");  
+         
+   var bt = new BridgeTalk;  
+   bt.target = "illustrator";  
+         
+   var script = des.read();  
+   des.close();  
+              
+   bt.body = script;  
+   bt.send();  
+  
+}// end function  
+  
+win.center();  
+win.show(); 
---
+++ organize layersa.jsx
@@ -0,0 +1,70 @@
+﻿cTID = function(s) { return app.charIDToTypeID(s); };
+sTID = function(s) { return app.stringIDToTypeID(s); };
+ 
+function newGroupFromLayers(doc) {
+    var desc = new ActionDescriptor();
+    var ref = new ActionReference();
+    ref.putClass( sTID('layerSection') );
+    desc.putReference( cTID('null'), ref );
+    var lref = new ActionReference();
+    lref.putEnumerated( cTID('Lyr '), cTID('Ordn'), cTID('Trgt') );
+    desc.putReference( cTID('From'), lref);
+    executeAction( cTID('Mk  '), desc, DialogModes.NO );
+};
+ 
+function undo() {
+   executeAction(cTID("undo", undefined, DialogModes.NO));
+};
+ 
+function getSelectedLayers(doc) {
+  var selLayers = [];
+  newGroupFromLayers();
+ 
+  var group = doc.activeLayer;
+  var layers = group.layers;
+ 
+  for (var i = 0; i < layers.length; i++) {
+    selLayers.push(layers[i]);
+  }
+ 
+  undo();
+ 
+  return selLayers;
+};
+ 
+var selectedLayers = getSelectedLayers(app.activeDocument);
+
+function printObj(obj) {
+    var key;
+    $.write("----------\n");
+    $.write("keys: \n\n");
+        for(key in obj) {
+                try{
+               $.write(key + " : " + obj[key].toString() +'\n'); 
+               }
+        catch(e){};
+        }
+    $.write("----------\n");
+};
+
+printObj(selectedLayers[0]);
+
+
+//app.activeDocument.activeLayer = selectedLayers[1];
+function print(i) {
+    if(i == "object"){ 
+        print("you are trying to printObject, use printObj(); function"); 
+    }
+    else {  
+        $.write(i+"\n"); 
+    }
+};
+
+selectedLayersIndex= [];
+for(i = 0; i < selectedLayers.length; i++){
+    selectedLayersIndex.push(selectedLayers[i].itemIndex+1);
+}
+
+print(selectedLayersIndex[1]);
+
+//alert(selectedLayers[2].name)
