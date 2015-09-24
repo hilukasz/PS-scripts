b521620916fd3962b15931e0351b1605fdae3090
Łukasz Wieczorek <lukasz@hellowoo.com>
2015-09-24 15:35:26 -0700

    new files

---
+++ Export Guides - Photoshop.jsx
@@ -0,0 +1,24 @@
+﻿if (documents.length == 0) {
+    alert("There are no documents open.");
+}
+else {
+    var docRef = activeDocument;
+    
+    
+    if ($.os.search(/windows/i) != -1) {
+    fileLineFeed = "Windows"
+    } else {
+    fileLineFeed = "Macintosh"
+    }
+
+    fileOut = new File("~/Desktop/Guides.txt")
+    fileOut.lineFeed = fileLineFeed
+    fileOut.open("w", "TEXT", "????")
+
+    for(var i=0; i<docRef.guides.length; ++i) {
+        fileOut.write(docRef.guides[i].direction + "," + docRef.guides[i].coordinate + "\n")
+    }
+    fileOut.close();
+    alert("Guides exported!");
+}
+
---
+++ Import Guides - Photoshop.jsx
@@ -0,0 +1,29 @@
+﻿if (documents.length == 0) {
+    alert("There are no documents open.");
+}
+else {
+    var docRef = activeDocument;
+    
+    
+    if ($.os.search(/windows/i) != -1) {
+    fileLineFeed = "Windows"
+    } else {
+    fileLineFeed = "Macintosh"
+    }
+
+    fileOut = new File("~/Desktop/Guides.txt")
+    fileOut.lineFeed = fileLineFeed
+    fileOut.open("r", "TEXT", "????")
+    var line = "";
+    while((line = fileOut.readln()) !== "")
+    {
+            var dir = line.split(",")[0];
+            var amt = line.split(",")[1].split(" ")[0];
+            var unit = line.split(",")[1].split(" ")[1];
+            var cmd = "docRef.guides.add("+dir+",UnitValue("+amt+","+"\""+unit+"\"))";
+            eval(cmd);
+    }
+    fileOut.close();
+    alert("Guides imported!");
+}
+
