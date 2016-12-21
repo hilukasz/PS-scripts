a1631920257ae77127b00ca573b344cc79c4ab8b
Łukasz Wieczorek <wutronic@Lukaszs-MacBook-Pro.local>
2016-12-21 12:14:10 -0800

    fix line ending

--- [wu]1440 icon maker - android.jsx
+++ [wu]1440 icon maker - android.jsx
@@ -1,1 +1,4 @@
-﻿var layerLength = app.activeDocument.layers.length;var docRef  = app.activeDocument;var myPrefix=prompt("enter prefix","").toLowerCase().replace(/ /g, "_");app.activeDocument.activeLayer.name = "drawable-xxxhdpi/"+myPrefix+", 75% drawable-xxhdpi/"+myPrefix+", 50% drawable-xhdpi/"+myPrefix+",  37.5% drawable-hdpi/"+myPrefix+", 25% drawable-mdpi/"+myPrefix+"" 
+﻿var layerLength = app.activeDocument.layers.length;
+var docRef  = app.activeDocument;
+var myPrefix=prompt("enter prefix","").toLowerCase().replace(/ /g, "_");
+app.activeDocument.activeLayer.name = "drawable-xxxhdpi/"+myPrefix+", 75% drawable-xxhdpi/"+myPrefix+", 50% drawable-xhdpi/"+myPrefix+",  37.5% drawable-hdpi/"+myPrefix+", 25% drawable-mdpi/"+myPrefix+"" 
